4d09ed5fdecf4b12fe3a46c3792159cdb08d2062
Andrei Muntean <andrei.muntean.dev@gmail.com>
2015-03-28 00:10:59 +0200

    Optimized the solution

--- Farey/Farey.java
+++ Farey/Farey.java
@@ -1,120 +1,202 @@
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 /**
- * Computes the kth element of the Farey sequence of order n.
+ * Computes the kth element of the Farey sequence of a specified order.
  *
  * @author Andrei Muntean
+ * @author Miriam Costan
  */
 public class Farey
 {
-	// Generates the Farey sequence of the specified order.
-	public static ArrayList<Fraction> generate(int order)
-	{
-		ArrayList<Fraction> sequence = new ArrayList<Fraction>(10 * order);
-		Fraction currentElement = new Fraction(0, 1);
+    // Counts the number of fractions which are smaller than the specified
+    // fraction and which have their denominators smaller than or equal to the
+    // specified denominator.
+    private static int countFractionsUpTo(int numerator, int denominator, boolean irreducibleOnly)
+    {
+        int result = 0;
 
-		// Adds the first value: 0/1.
-		sequence.add(currentElement);
+        // fractionCount[index] = the number of fractions with the denominator
+        // "index" which are smaller than the specified fraction.
+        int[] fractionCount = new int[denominator + 1];
 
-		while (!currentElement.isUnit())
-		{
-			int nextNominator = 1;
-			int nextDenominator = 1;
+        // The maximum numerator of each fraction with the denominator "i" is
+        // "i". Therefore there are Math.floor(i * numerator / denominator)
+        // fractions which are smaller than the specified fraction and have
+        // the denominator equal to "i".
+        // 
+        // Example. Given "4/8":
+        // i = 2:  1/2 -- fractionCount[2] = 1.
+        // i = 3:  1/3 -- fractionCount[3] = 1.
+        // i = 4:  1/4 2/4 -- fractionCount[4] = 2.
+        // i = 5:  1/5 2/5 -- fractionCount[5] = 2.
+        // i = 6:  1/6 2/6 3/6 -- fractionCount[6] = 3.
+        // i = 7:  1/7 2/7 3/7 -- fractionCount[7] = 3.
+        // i = 8:  1/8 2/8 3/8 4/8 -- fractionCount[8] = 4.
+        for (int currentDenominator = 2; currentDenominator <= denominator; ++currentDenominator)
+        {
+            fractionCount[currentDenominator] = currentDenominator * numerator / denominator;
+        }
 
-			for (int denominator = order; denominator >= 1; --denominator)
-			{
-				int ad = currentElement.getNominator() * denominator;
-				
-				for (int nominator = 1; nominator <= denominator; ++nominator)
-				{
-					int bc = currentElement.getDenominator() * nominator;
+        // Determines whether the fractions which can be reduced should be forgotten.
+        if (irreducibleOnly)
+        {
+            for (int currentDenominator = 2; currentDenominator <= denominator; ++currentDenominator)
+            {
+                for (int multiplier = 2 * currentDenominator; multiplier <= denominator; multiplier += currentDenominator)
+                {
+                    // In fractionCount[multiplier], forgets the fractions from
+                    // fractionCount[currentDenominator] since they are repeating
+                    // themselves.
+                    fractionCount[multiplier] -= fractionCount[currentDenominator];
+                }
+            }
+        }
 
-					// Given a/b and c/d two Farey neighbors, ad - bc is -1.
-					if (ad - bc == -1)
-					{
-						// Determines whether the currently stored potential neighbor is greater than
-						// the new neighbor that can be created.
-						if (nextNominator * denominator > nextDenominator * nominator)
-						{
-							// The actual Farey neighbor is the smallest possible neighbor.
-							nextNominator = nominator;
-							nextDenominator = denominator;
-						}
-					}
-				}
-			}
+        for (int currentDenominator = 2; currentDenominator <= denominator; ++currentDenominator)
+        {
+            result += fractionCount[currentDenominator];
+        }
 
-			sequence.add(currentElement = new Fraction(nextNominator, nextDenominator));
-		}
+        return result;
+    }
 
-		return sequence;
-	}
+    // Gets a nominator from the Farey sequence such that the kth fraction is in the
+    // interval [neighbor / order, (neighbor + 1) / order].
+    private static int findNeighbor(int k, int order)
+    {
+        int left = 1;
+        int right = order - 1;
 
-	// Computes the kth element of the Farey sequence of order n.
-	// To be used if the other elements do not need to be stored.
-	public static Fraction get(int k, int order)
-	{
-		int currentNominator = 0;
-		int currentDenominator = 1;
+        while (left <= right)
+        {
+            int middle = (left + right) / 2;
 
-		while (k-- > 0)
-		{
-			int nextNominator = 1;
-			int nextDenominator = 1;
+            if (k > countFractionsUpTo(middle, order, true))
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle - 1;
+            }
+        }
 
-			for (int denominator = order; denominator >= 1; --denominator)
-			{
-				int ad = currentNominator * denominator;
-				
-				for (int nominator = 1; nominator <= denominator; ++nominator)
-				{
-					int bc = currentDenominator * nominator;
+        // "right" is now the "neighbor" described above.
+        return right;
+    }
 
-					// Given a/b and c/d two Farey neighbors, ad - bc is -1.
-					if (ad - bc == -1)
-					{
-						// Determines whether the currently stored potential neighbor is greater than
-						// the new neighbor that can be created.
-						if (nextNominator * denominator > nextDenominator * nominator)
-						{
-							// The actual Farey neighbor is the smallest possible neighbor.
-							nextNominator = nominator;
-							nextDenominator = denominator;
-						}
-					}
-				}
-			}
+    // Computes the kth element of the Farey sequence of a specified order.
+    public static Fraction get(int k, int order)
+    {
+        if (--k == 0)
+        {
+            return new Fraction(0, 1);
+        }
 
-			currentNominator = nextNominator;
-			currentDenominator = nextDenominator;
-		}
+        int neighbor = findNeighbor(k, order);
 
-		return new Fraction(currentNominator, currentDenominator);
-	}
+        // Represents the list of fractions from the interval
+        // [neighbor / order, (neighbor + 1) / order].
+        TreeSet<Fraction> fractions = new TreeSet<Fraction>();
 
-	// Computes the kth element of the Farey sequence of order n.
-	public static void main(String[] args) throws IOException
-	{
-		int n;
-		int k;
-		int index = 0;
+        // K becomes significantly smaller as it is reduced to the elements
+        // from the interval [neighbor / order, (neighbor + 1) / order].
+        k -= countFractionsUpTo(neighbor, order, true);
 
-		try (Scanner scanner = new Scanner(new FileReader("farey.in")))
-		{
-			n = scanner.nextInt();
-			k = scanner.nextInt();
-		}
+        // Knowing that each fraction has a unique denominator, finds every
+        // fraction from the specified interval.
+        for (int currentDenominator = 2; currentDenominator <= order; ++currentDenominator)
+        {
+            int numerator = currentDenominator * (neighbor + 1) / order;
 
-		Fraction kthElement = get(k, n);
+            if (currentDenominator * neighbor < numerator * order)
+            {
+                // Sorts the fractions and eliminates duplicates by storing
+                // them in a tree set.
+                fractions.add(new Fraction(numerator, currentDenominator));
+            }
+        }
 
-		try (PrintWriter printWriter = new PrintWriter(new FileWriter("farey.out")))
-		{
-			printWriter.printf("%s ", kthElement);
-		}
-	}
+        // Gets the kth element from the [neighbor / order, (neighbor + 1) / order]
+        // interval of distinct fractions.
+        for (Fraction fraction : fractions)
+        {
+            if (--k == 0)
+            {
+                return fraction;
+            }
+        }
+
+        return null;
+    }
+
+    // Generates the Farey sequence of a specified order.
+    public static ArrayList<Fraction> generate(int order)
+    {
+        ArrayList<Fraction> sequence = new ArrayList<Fraction>(10 * order);
+        Fraction currentElement = new Fraction(0, 1);
+
+        // Adds the first value: 0/1.
+        sequence.add(currentElement);
+
+        while (!currentElement.isUnit())
+        {
+            int nextNominator = 1;
+            int nextDenominator = 1;
+
+            for (int denominator = order; denominator >= 1; --denominator)
+            {
+                int ad = currentElement.getNominator() * denominator;
+                
+                for (int nominator = 1; nominator <= denominator; ++nominator)
+                {
+                    int bc = currentElement.getDenominator() * nominator;
+
+                    // Given a/b and c/d two Farey neighbors, ad - bc is -1.
+                    if (ad - bc == -1)
+                    {
+                        // Determines whether the currently stored potential neighbor is greater than
+                        // the new neighbor that can be created.
+                        if (nextNominator * denominator > nextDenominator * nominator)
+                        {
+                            // The actual Farey neighbor is the smallest possible neighbor.
+                            nextNominator = nominator;
+                            nextDenominator = denominator;
+                        }
+                    }
+                }
+            }
+
+            sequence.add(currentElement = new Fraction(nextNominator, nextDenominator));
+        }
+
+        return sequence;
+    }
+
+    // Computes the kth element of the Farey sequence of a specified order;
+    public static void main(String[] args) throws IOException
+    {
+        int order;
+        int k;
+
+        if (args.length == 2)
+        {
+            order = Integer.parseInt(args[0]);
+            k = Integer.parseInt(args[1]);
+        }
+        else
+        {
+            try (Scanner scanner = new Scanner(System.in))
+            {
+                order = scanner.nextInt();
+                k = scanner.nextInt();
+            }
+        }
+
+        // Outputs the kth element of the Farey sequence of the specified order.
+        System.out.printf("%s", get(k, order));
+    }
 }
--- Farey/Fraction.java
+++ Farey/Fraction.java
@@ -5,114 +5,114 @@
  */
 public class Fraction implements Comparable<Fraction>
 {
-	private final int nominator;
-	private final int denominator;
+    private final int nominator;
+    private final int denominator;
 
-	/**
-	 * Constructs a fraction.
-	 *
-	 * @param nominator The nominator.
-	 * @param denominator The denominator.
-	 */
-	public Fraction(int nominator, int denominator) throws IllegalArgumentException
-	{
-		if (denominator == 0)
-		{
-			throw new IllegalArgumentException("Denominator cannot be zero.");
-		}
+    /**
+     * Constructs a fraction.
+     *
+     * @param nominator The nominator.
+     * @param denominator The denominator.
+     */
+    public Fraction(int nominator, int denominator) throws IllegalArgumentException
+    {
+        if (denominator == 0)
+        {
+            throw new IllegalArgumentException("Denominator cannot be zero.");
+        }
 
-		this.nominator = nominator;
-		this.denominator = denominator;
-	}
+        this.nominator = nominator;
+        this.denominator = denominator;
+    }
 
-	/**
-	 * Gets the nominator.
-	 *
-	 * @return The nominator.
-	 */
-	public int getNominator()
-	{
-		return nominator;
-	}
+    /**
+     * Gets the nominator.
+     *
+     * @return The nominator.
+     */
+    public int getNominator()
+    {
+        return nominator;
+    }
 
-	/**
-	 * Gets the denominator.
-	 *
-	 * @return The denominator.
-	 */
-	public int getDenominator()
-	{
-		return denominator;
-	}
+    /**
+     * Gets the denominator.
+     *
+     * @return The denominator.
+     */
+    public int getDenominator()
+    {
+        return denominator;
+    }
 
-	/**
-	 * Determines whether this fraction is equal to 1.
-	 *
-	 * @return True if this fraction is equal to 1.
-	 */
-	public boolean isUnit()
-	{
-		return nominator == denominator;
-	}
+    /**
+     * Determines whether this fraction is equal to 1.
+     *
+     * @return True if this fraction is equal to 1.
+     */
+    public boolean isUnit()
+    {
+        return nominator == denominator;
+    }
 
-	/**
-	 * Compares this fraction with the specified fraction.
-	 *
-	 * @param A fraction.
-	 *
-	 * @return A negative integer, zero or a positive integer if this fraction is
-	 * less than, equal to or greater than the specified fraction.
-	 */
-	public int compareTo(Fraction fraction)
-	{
-		return compareTo(fraction.getNominator(), fraction.getDenominator());
-	}
+    /**
+     * Compares this fraction with the specified fraction.
+     *
+     * @param A fraction.
+     *
+     * @return A negative integer, zero or a positive integer if this fraction is
+     * less than, equal to or greater than the specified fraction.
+     */
+    public int compareTo(Fraction fraction)
+    {
+        return compareTo(fraction.getNominator(), fraction.getDenominator());
+    }
 
-	/**
-	 * Compares this fraction with the specified integers which represent a fraction.
-	 *
-	 * @param The nominator of a fraction.
-	 * @param The denominator of a fraction.
-	 *
-	 * @return A negative integer, zero or a positive integer if this fraction is
-	 * less than, equal to or greater than the specified fraction.
-	 */
-	public int compareTo(int nominator, int denominator)
-	{
-		// (a/b <relation> c/d) iff (ad <relation> bc)
-		long ad = this.nominator * denominator;
-		long bc = this.denominator * nominator;
+    /**
+     * Compares this fraction with the specified integers which represent a fraction.
+     *
+     * @param The nominator of a fraction.
+     * @param The denominator of a fraction.
+     *
+     * @return A negative integer, zero or a positive integer if this fraction is
+     * less than, equal to or greater than the specified fraction.
+     */
+    public int compareTo(int nominator, int denominator)
+    {
+        // (a/b <relation> c/d) iff (ad <relation> bc)
+        long ad = this.nominator * denominator;
+        long bc = this.denominator * nominator;
 
-		if (ad < bc)
-		{
-			return -1;
-		}
-		else if (ad == bc)
-		{
-			return 0;
-		}
-		else
-		{
-			return 1;
-		}
-	}
+        if (ad < bc)
+        {
+            return -1;
+        }
+        else if (ad == bc)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
 
-	public boolean equals(int nominator, int denominator)
-	{
-		return this.nominator == nominator && this.denominator == denominator;
-	}
+    public boolean equals(int nominator, int denominator)
+    {
+        return this.nominator == nominator && this.denominator == denominator;
+    }
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		Fraction fraction = (Fraction)obj;
+    @Override
+    public boolean equals(Object obj)
+    {
+        Fraction fraction = (Fraction)obj;
 
-		return nominator == fraction.getNominator() && denominator == fraction.getDenominator();
-	}
+        return nominator == fraction.getNominator() && denominator == fraction.getDenominator();
+    }
 
-	@Override
-	public String toString()
-	{
-		return nominator + "/" + denominator;
-	}
+    @Override
+    public String toString()
+    {
+        return nominator + "/" + denominator;
+    }
 }
